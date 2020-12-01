950d26a7f9266ce169be1988c7a3617d0c2dfde1
Pawel Sakaluk <paul.jackals@gmail.com>
2020-12-01 23:29:21 +0100

    removed unnecessary engine

--- src/ex2_car/car.py
+++ src/ex2_car/car.py
@@ -1,18 +1,9 @@
-from .engine import Engine
-
-
 class Car:
-    def __init__(self, engine=None):
-        if engine:
-            self.engine = Engine()
-        else:
-            self.engine = engine
-
     def needsFuel(self):
         pass
 
     def getEngineTemperature(self):
-        return self.engine.getTemperature()
+        pass
 
     def driveTo(self, destination):
         pass
--- tests/test_ex2_car.py
+++ tests/test_ex2_car.py
@@ -1,30 +1,31 @@
 import unittest
 from unittest.mock import *
 from ex2_car.car import Car
-from ex2_car.engine import Engine
 
 
 class TestCar(unittest.TestCase):
+    def setUp(self):
+        self.car = Car()
+
     @patch.object(Car, 'needsFuel')
     def test_needsFuel(self, mock_method):
         mock_method.return_value = False
-        car = Car()
-        self.assertFalse(car.needsFuel())
+        self.assertFalse(self.car.needsFuel())
 
     @patch.object(Car, 'driveTo')
     def test_driveTo(self, mock_method):
         mock_method.side_effect = lambda x: 'Driving to ' + x
-        car = Car()
 
         city = 'Warsaw'
-        self.assertEqual(car.driveTo(city), 'Driving to ' + city)
+        self.assertEqual(self.car.driveTo(city), 'Driving to ' + city)
 
-    @patch.object(Engine, 'getTemperature')
+    @patch.object(Car, 'getEngineTemperature')
     def test_getEngineTemperature(self, mock_method):
         mock_method.return_value = 40
-        engine = Engine()
-        car = Car(engine)
-        self.assertEqual(car.getEngineTemperature(), 40)
+        self.assertEqual(self.car.getEngineTemperature(), 40)
+
+    def tearDown(self):
+        self.car = None
 
 
 if __name__ == '__main__':
